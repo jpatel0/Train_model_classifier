6eae2adf18bf024730846fbc72e89d978c43651c
jpatel0 <jaypatel9670@gmail.com>
2019-06-22 19:04:59 +0530

    base prototype ready

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template, request,redirect,url_for,send_file
+from flask import Flask, render_template, request,redirect,url_for,send_file, jsonify
 from flask_mail import Mail, Message
 import os
-
-# from keras.models import load_model
-# from keras.utils import CustomObjectScope
-# from keras.initializers import glorot_uniform
+import logging
 
 if("upfolders" not in os.listdir("./static/")):
     os.mkdir("static/upfolders")
@@ -12,41 +9,69 @@ if("upfolders" not in os.listdir("./static/")):
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join("static","upfolders")
 
-with open('./emailenv.env','r') as f:
-    global email
-    global pwd
-    data = f.read()
-    email,pwd = data.split("#")
-    print(len(email))
+global log
+log= ""
 
-mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": email,
-    "MAIL_PASSWORD": pwd
-}
+def init_smtp():
+    with open('./emailenv.env','r') as f:
+        global email
+        global pwd
+        data = f.read()
+        email,pwd = data.split("#")
+        print(len(email))
 
-app.config.update(mail_settings)
-mail = Mail(app)
+    
+    mail_settings = {
+        "MAIL_SERVER": 'smtp.gmail.com',
+        "MAIL_PORT": 465,
+        "MAIL_USE_TLS": False,
+        "MAIL_USE_SSL": True,
+        "MAIL_USERNAME": email,
+        "MAIL_PASSWORD": pwd
+    }
+    global mail
+    app.config.update(mail_settings)
+    mail = Mail(app)
 
-mail.Message
+def setup_logging():
+    if not app.debug:
+        # In production mode, add log handler to sys.stderr.
+        app.logger.addHandler(logging.StreamHandler())
+        app.logger.setLevel(logging.INFO)
+
 # def init():
 
-def train_model(folder_path):
+@app.route('/trainProgress')
+def trainProgress():
+    global log
+    if(log):
+        return jsonify({"Progress":log})
+    else:
+        return jsonify({"Progress":""})
+
+@app.route('/download_model/<filename>')
+def download_model(filename):
+    print(filename)
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'],filename),as_attachment=True,attachment_filename='model.h5')
+
+@app.route('/train_model',methods=['POST','GET'])
+def train_model():
     print('innn')
     import tensorflow as tf
-    from tensorflow import keras    
+    from tensorflow import keras
     from PIL import Image
     import numpy as np
     import gc
 
+    global log
+    print(request.json['folder'])
+    folder_name = request.json['folder']
+    log = r"Initializing variables...."
     gc.enable()
-    base_dir = folder_path
+    base_dir = os.path.join(app.config['UPLOAD_FOLDER'],folder_name)
     train_dir = os.path.join(base_dir, 'train')
     validation_dir = os.path.join(base_dir, 'validation')
-    image_size = 160 
+    image_size = 160
     batch_size = 32
 
     train_datagen = keras.preprocessing.image.ImageDataGenerator(
@@ -76,10 +101,11 @@ def train_model(folder_path):
     model.compile(optimizer=keras.optimizers.RMSprop(lr=0.0001),
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-    epochs = 10
+    epochs = 1
     steps_per_epoch = train_generator.n // batch_size
     validation_steps = validation_generator.n // batch_size
     print("training started")
+    log = r"Training the Model."
     model.fit_generator(train_generator,
                                 steps_per_epoch = steps_per_epoch,
                                 epochs=epochs,
@@ -88,6 +114,7 @@ def train_model(folder_path):
                                 validation_steps=validation_steps)
 
     fine_tune_at = 100
+    log =  r"Dense Layer's Trainig done:."
 
     # Freeze all the layers before the `fine_tune_at` layer
     for layer in base_model.layers[:fine_tune_at]:
@@ -97,17 +124,23 @@ def train_model(folder_path):
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-    model.fit_generator(train_generator,
+    
+    log = log + r'Fine Tuning the model, beginning to train.'
+    hist = model.fit_generator(train_generator,
                                    steps_per_epoch = steps_per_epoch,
                                    epochs=epochs,
                                    workers=4,
                                    validation_data=validation_generator,
                                    validation_steps=validation_steps)
 
-    model_path = folder_path+'_model_without_fine_tune.h5'
+    
+    log = r'Model is done training,Acc:{}, Loss:{}, Val_acc:{}, Val_loss:{}'.format(hist.history['acc'],hist.history['loss'],hist.history['val_acc'],hist.history['val_loss'])
+    model_path = base_dir + '_model_without_fine_tune.h5'
     model.save(model_path)
-    gc.collect()
-    return model_path
+    # gc.collect()
+    print(model_path)
+    print(folder_name + '_model_without_fine_tune.h5')
+    return jsonify({"success":True,"modelLink": folder_name + '_model_without_fine_tune.h5'})
 
 @app.route("/")
 def index():
@@ -118,33 +151,67 @@ def upload():
     return render_template("upload.html")
 
 
-@app.route("/test",methods=["POST","GET"])
-def test():
-    accept_ext = ['zip','rar','7zip']
-    if('zipfile' in request.files and any(True for ext in accept_ext if ext in request.files['zipfile'].filename)):
-        import zipfile
-        file = request.files['zipfile']
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'],file.filename)
-        file.save(filepath)
-        zip = zipfile.ZipFile(filepath)
-        zip.extractall(path=app.config['UPLOAD_FOLDER'])
-        zip.close()
-        os.remove(filepath)
-        folder_name = os.path.splitext(filepath)[0]
-        print(folder_name)
-        model_path = train_model(folder_name)
-        return send_file(model_path,as_attachment=True,attachment_filename='model.h5')
-    else:
-        return "wrong uploaded file"
+@app.route("/uploadFile",methods=['POST'])
+def uploadFile(): 
+    import zipfile
+    file = request.files['zipfile']
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'],file.filename)
+    file.save(filepath)
+    zip = zipfile.ZipFile(filepath)
+    zip.extractall(path=app.config['UPLOAD_FOLDER'])
+    zip.close()
+    os.remove(filepath)
+    folder_name = os.path.splitext(filepath)[0]
+    return jsonify({"redirect":True,
+                    "redirectLink":"./start/"+os.path.splitext(file.filename)[0]})
 
 
-if __name__ == "__main__":
-    # init()
-    app.run(host="0.0.0.0",debug=True)
-    # train_model()
+@app.route('/start/<folder_name>')
+def startTrain(folder_name):
+    # path = train_model(os.path.join(app.config['UPLOAD_FOLDER'],folder_name))
+    return render_template('startTrain.html')
+    
+
+# def unzipAndTrain(file):
+#     import zipfile
+#     file = request.files['zipfile']
+#     filepath = os.path.join(app.config['UPLOAD_FOLDER'],file.filename)
+#     file.save(filepath)
+#     zip = zipfile.ZipFile(filepath)
+#     zip.extractall(path=app.config['UPLOAD_FOLDER'])
+#     zip.close()
+#     os.remove(filepath)
+#     folder_name = os.path.splitext(filepath)[0]
+#     print(folder_name)
+#     global log
+#     log = log + r'Folders extraction complete.'
+#     model_path = train_model(folder_name)
+
+
+# @app.route("/test",methods=["POST","GET"])
+# def test():
+#     accept_ext = ['zip','rar','7zip']
+#     if('zipfile' in request.files and any(True for ext in accept_ext if ext in request.files['zipfile'].filename)):
+#         file = request.files['zipfile']
+#         global log
+#         log = r"File uploaded.. extracting...."
+#         unzipAndTrain(file)
+#         return render_template('test.html')
+#         # return send_file(model_path,as_attachment=True,attachment_filename='model.h5')
+#     else:
+#         return "wrong uploaded file"
+
+
+def send_msg():
     with app.app_context():
         msg = Message(subject="Hello",
                       sender=app.config.get("MAIL_USERNAME"),
                       recipients=["jaypatel9670@gmail.com"], 
                       body="This is a test email I sent with Gmail and Python!")
         mail.send(msg)
+
+if __name__ == "__main__":
+    # init()
+    app.run(host="0.0.0.0",debug=True)
+    # train_model()
+    
